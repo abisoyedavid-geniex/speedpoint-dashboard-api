5449b6b87dd54f1ed514b85caa7268b94ef0cb45
Abisoye Oke-Lawal <abiso.lawal@gmail.com>
2025-11-17 20:58:46 +0100

    - Add leads route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-const ticketsRoutes = require("./tickets");
-const healthRoutes = require("./health");
+const leadsRoutes = require('./leads');
+const ticketsRoutes = require('./tickets');
+const healthRoutes = require('./health');
 
 module.exports = {
   healthRoutes,
+  leadsRoutes,
   ticketsRoutes,
 };
